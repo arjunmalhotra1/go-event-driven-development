45c34c20de4aa55a651bd96e13e4f2386438b265
arjunmalhotra1 <am2cj@virginia.edu>
2024-06-30 11:54:56 -0400

    use event processor 9.4

---
+++ 09-cqrs-events/04-event-processor/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill/components/cqrs"
+	"github.com/ThreeDotsLabs/watermill/message"
+)
+
+func RegisterEventHandlers(
+	sub message.Subscriber,
+	router *message.Router,
+	handlers []cqrs.EventHandler,
+	logger watermill.LoggerAdapter,
+) error {
+	config := cqrs.EventProcessorConfig{
+		SubscriberConstructor: func(params cqrs.EventProcessorSubscriberConstructorParams) (message.Subscriber, error) {
+			return sub, nil
+		},
+		GenerateSubscribeTopic: func(params cqrs.EventProcessorGenerateSubscribeTopicParams) (string, error) {
+			return params.EventName, nil
+		},
+		Marshaler: cqrs.JSONMarshaler{
+			GenerateName: cqrs.StructName,
+		},
+		Logger: logger,
+	}
+	ep, err := cqrs.NewEventProcessorWithConfig(router, config)
+	if err != nil {
+		return err
+	}
+
+	return ep.AddHandlers(handlers...)
+
+}
