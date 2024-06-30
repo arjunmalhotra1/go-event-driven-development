11721912540bf3644515af18a3ffca2de4a4a742
arjunmalhotra1 <am2cj@virginia.edu>
2024-06-29 21:27:29 -0400

    implement the cqrs event bus - ex 9.1

---
+++ 09-cqrs-events/01-event-bus/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/ThreeDotsLabs/watermill/components/cqrs"
+	"github.com/ThreeDotsLabs/watermill/message"
+)
+
+func NewEventBus(pub message.Publisher) (*cqrs.EventBus, error) {
+	config := cqrs.EventBusConfig{
+		GeneratePublishTopic: func(params cqrs.GenerateEventPublishTopicParams) (string, error) {
+			return params.EventName, nil
+		},
+		Marshaler: cqrs.JSONMarshaler{},
+	}
+	return cqrs.NewEventBusWithConfig(pub, config)
+}
