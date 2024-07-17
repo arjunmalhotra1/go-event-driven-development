1e8d23f674f3031e3b2697bc5c2e392688807975
arjunmalhotra1 <am2cj@virginia.edu>
2024-07-16 20:12:43 -0400

    Add log.FromContext to RemoveCanceledTicket & SaveTicketToDatabase

--- project/message/event/remove_cancelled_ticket.go
+++ project/message/event/remove_cancelled_ticket.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"fmt"
 	"tickets/entities"
+
+	"github.com/ThreeDotsLabs/go-event-driven/common/log"
 )
 
 func (h Handler) RemoveCanceledTicket(ctx context.Context, event *entities.TicketBookingCanceled) error {
+	log.FromContext(ctx).Info("Removing cancelled tickets from the database")
+
 	err := h.ticketsRepository.Delete(ctx, event.TicketID)
 	if err != nil {
 		return fmt.Errorf("failed to delete the ticket from the database %w", err)
--- project/message/event/save-ticket-db.go
+++ project/message/event/save-ticket-db.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"fmt"
 	"tickets/entities"
+
+	"github.com/ThreeDotsLabs/go-event-driven/common/log"
 )
 
 func (h Handler) SaveTicketToDatabase(ctx context.Context, event *entities.TicketBookingConfirmed) error {
+
+	log.FromContext(ctx).Info("Saving ticket to the database")
 
 	err := h.ticketsRepository.Add(ctx, entities.Ticket{
 		TicketID:      event.TicketID,
